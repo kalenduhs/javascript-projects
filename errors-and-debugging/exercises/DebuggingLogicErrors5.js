0f850be8bacacf9741e6216785561a112af82d2d
kalenduhs <kalenduhs@gmail.com>
2023-09-13 12:07:54 -0500

    Finished Exercises

--- errors-and-debugging/exercises/DebuggingLogicErrors5.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors5.js
@@ -2,20 +2,21 @@
 // Since the issue is with launchReady, ONE way to fix the logic error is to use a different variable to store the fuel check result. 
 // Refactor the code to do this. Verify that your change works by updating the console.log statements.
 
+let fuelStatus = false;
 let launchReady = false;
-let fuelLevel = 17000;
+let fuelLevel = 24000;
 let crewStatus = true;
 let computerStatus = 'green';
 
 if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
-   launchReady = true;
+   fuelStatus = true;
 } else {
    console.log('WARNING: Insufficient fuel!');
-   launchReady = false;
+   fuelStatus = false;
 }
 
-console.log("launchReady = ", launchReady);
+console.log("fuelStatus = ", fuelStatus);
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
@@ -26,3 +27,9 @@ if (crewStatus && computerStatus === 'green'){
 }
 
 console.log("launchReady = ", launchReady);
+
+if (fuelStatus === true && launchReady === true) {
+   console.log("Liftoff!");
+} else {
+   console.log("Launch scrubbed");
+}

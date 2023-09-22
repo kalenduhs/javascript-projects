5240c2e5d731f14e503653121412e642844b0f3e
kalenduhs <kalenduhs@gmail.com>
2023-09-21 20:27:17 -0500

    Attempted Studio

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -2,18 +2,26 @@ const input = require('readline-sync');
 
 // Part A: #1 Populate these arrays
 
-let protein = [];
-let grains = [];
-let veggies = [];
-let beverages = [];
-let desserts = [];
+let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
+let grains = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
+let veggies = ['peas', 'green beans', 'kale', 'edamame', 'broccoli', 'asparagus'];
+let beverages = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
+let desserts = ['apple', 'banana', 'more kale', 'ice cream', 'chocolate', 'kiwi'];
 
 
 function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   let pantry = [protein, grains, veggies, beverages, desserts];
   let meals = [];
+  let meal =[];
   
   /// Part A #2: Write a ``for`` loop inside this function
+  for (let i = 0; i < numMeals; i++) {
+    meals.push(meal[i]);
+    for (let j= 0; j < 4; j++){
+      meal.unshift(pantry[i][j]);
+    }
+  }
+
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
 
 
@@ -45,8 +53,8 @@ function runProgram() {
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
   
-  // let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
-  // console.log(meals)
+  let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
+  console.log(meals)
   
 
   /// TEST PART B HERE ///

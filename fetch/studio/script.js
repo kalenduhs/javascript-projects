fb458ef3253fd0f6c0a90d875850b406d5efd4e3
kalenduhs <kalenduhs@gmail.com>
2023-11-06 20:26:11 -0600

    Completed Fetch Studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,25 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            //console.log(json);
+            const container = document.getElementById("container");
+            let htmlStuff = '';
+            json.sort((a, b) => b.hoursInSpace - a.hoursInSpace);
+            for (let i = 0; i < json.length; i++) {
+            htmlStuff += `<div class="astronaut">
+            <div class="bio">
+                <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                <ul>
+                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                <li>Active: ${json[i].active}</li>
+                <li>Skills: ${json[i].skills}</li>
+                </ul>
+            </div>
+            <img class="avatar" src=${json[i].picture}>
+        </div>`;
+            }
+            container.innerHTML = htmlStuff;
+        });
+    });
+})
